9b68c042dedc744f87f2eb6311784a48c06665b9
zcittadin <z_cittadin@hotmail.com>
2022-01-12 07:56:19 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mqtt-app-test',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
