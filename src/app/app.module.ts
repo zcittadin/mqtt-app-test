fba47e598144aac3a0bb1e9598d22a2e8ba2ed9d
zcittadin <z_cittadin@hotmail.com>
2022-01-13 07:31:43 -0300

    Configuração da conexão com o broker e subscrição de tópicos para teste.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,24 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
+import { IMqttMessage, MqttModule, IMqttServiceOptions } from 'ngx-mqtt';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+
+export const MQTT_CONFIG: IMqttServiceOptions = {
+  hostname: 'test.mosquitto.org',
+  port: 8081,
+  protocol: "wss",
+  path: '/mqtt'
+};
+
+//const MQTT_CONFIG: IMqttServiceOptions = { connectOnCreate: false };
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, MqttModule.forRoot(MQTT_CONFIG)],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Observable, Subscription, Subject } from 'rxjs';
+import { IMqttMessage, MqttService } from 'ngx-mqtt';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnDestroy {
 
-  constructor() {}
+  private readonly onDestroy = new Subject<void>();
+
+  private subscription: Subscription;
+  public temp: string = "00,0";
+  public humid: string = "00,0";
+  public press: string = "00,0";
+
+  constructor(private mqtt: MqttService) {
+    this.subscription = this.mqtt.observe('my_topic/humid').subscribe((message: IMqttMessage) => {
+      this.humid = message.payload.toString();
+      console.log(this.humid);
+    });
+    this.subscription = this.mqtt.observe('my_topic/temp').subscribe((message: IMqttMessage) => {
+      this.temp = message.payload.toString();
+      console.log(this.temp);
+    });
+    this.subscription = this.mqtt.observe('my_topic/press').subscribe((message: IMqttMessage) => {
+      this.press = message.payload.toString();
+      console.log(this.press);
+    });
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+    this.onDestroy.next();
+    this.onDestroy.complete();
+  }
 
 }
